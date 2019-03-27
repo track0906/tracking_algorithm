3c67c1cee32459eb0dd9db2c07be04421663f531
track0906 <48977652+track0906@users.noreply.github.com>
2019-03-27 16:45:47 +0900

    add mp4

--- animation.py
+++ animation.py
@@ -121,7 +121,8 @@ class Animation_robot():
         shuold_save_animation = int(input())
 
         if shuold_save_animation: 
-            animation.save('basic_animation.gif', writer='imagemagick')
+            animation.save('basic_animation.mp4', writer="ffmpeg")
+            # animation.save('basic_animation.gif', writer='imagemagick')
 
         plt.show()
 
